f3db88137ebde1b23e3f4d933ebe9afcd71f439b
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-09-04 08:51:57 +0530

    Added: getMessage function to get a message by extendinf to GreetingService

---
+++ GreetingController.php
@@ -0,0 +1,18 @@
+<?php
+require_once("GreetingServices.php");
+class GreetingController extends GreetingService {
+    
+    function getMessage(){
+        echo $this->greetMessage();
+}
+// $db = new Database;
+// if($_SERVER['REQUEST_METHOD'] == "GET"){
+//     $db->getAllUsers();
+
+// } else if ($_SERVER['REQUEST_METHOD'] == "POST"){
+//     echo "POST";
+// } else {
+//     http_response_code(405);
+// }
+
+}
