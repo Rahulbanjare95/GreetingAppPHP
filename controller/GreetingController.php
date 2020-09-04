f37244a540cbe4d445cc9952804a82c0a4de0fa3
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-09-04 11:13:53 +0530

    Modified: function to save the greeting message in database by displaying a message

--- controller/GreetingController.php
+++ controller/GreetingController.php
@@ -1,19 +1,14 @@
 <?php
-require_once("GreetingServices.php");
+include_once 'model/GreetingServices.php';
 
-class GreetingController extends GreetingService {
-    
-    function getMessage($firstname,$lastname){
-        echo $this->greetMessage($firstname,$lastname);
-}
-// $db = new Database;
-// if($_SERVER['REQUEST_METHOD'] == "GET"){
-//     $db->getAllUsers();
-
-// } else if ($_SERVER['REQUEST_METHOD'] == "POST"){
-//     echo "POST";
-// } else {
-//     http_response_code(405);
-// }
-
+class GreetingController extends GreetingService
+{
+    function getMessage($firstname, $lastname)
+    {
+        echo $this->greetMessage($firstname, $lastname);
+    }
+    function storeMessageInRepository($firstName,$lastName){
+        $result = $this->addUsers($firstName,$lastName);
+        echo "message saved in database'.'<br>';
+    }
 }
