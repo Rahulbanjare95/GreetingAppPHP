1a2b73a5cf6a08dbfe7a74bd9d7381feb0e7abc3
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-09-04 09:14:03 +0530

    Modified: Greeting service to five message based on first and last name or only last name or hello world if nothing provided

--- GreetingServices.php
+++ GreetingServices.php
@@ -2,10 +2,21 @@
 class GreetingService {
 
     private $message;
+    public $firstName;
+    public $lastName;
 
-   function greetMessage(){
-       $this->message ="hello world!";
-       return $this->message;
+   function greetMessage($firstName, $lastName){
+       if ( $this->firstName = $firstName  ) {
+            $this->message ="hello ".$firstName;
+            return $this->message;
+       }
+       elseif ($this->firstName = $firstName && $this->lastName = $lastName){
+            $this->message ="hello ".$firstName." ".$lastName;
+            return $this->message;
+       }else{
+        $this->message ="hello world!";
+        return $this->message;
+       }
 }
 }
 
